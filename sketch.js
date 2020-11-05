9faf45f443182dcaf320ca32b6e6a47a74f15fa5
varshaOmg <72698225+varshaOmg@users.noreply.github.com>
2020-11-05 19:07:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -49,7 +49,9 @@ function draw() {
   rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.y= packageBody.position.y
+	
+  keyPressed()
   drawSprites();
  
 }
